36ffe40afeda99f12212d4eeed5de2dc3e1b050c
DavidMurillo32 <dasan46@outlook.com>
2023-02-23 09:40:20 -0500

    Final

--- ecuaciones.js
+++ ecuaciones.js
@@ -1,17 +1,33 @@
 
-function ecuaciones(){
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
 
-    var Res1 = parseFloat(document.variables.R1.value);
-    var Res2 = parseFloat(document.variables.R1.value);
-    var ResC = parseFloat(document.variables.R1.value);
-    var ResE = parseFloat(document.variables.R1.value);
-    var VCC = parseFloat(document.variables.R1.value);
+    const R1str = urlParams.get("R1")
+    const R1 = parseInt(R1str)
+    console.log(R1);
+    const R2str = urlParams.get("R2")
+    const R2 = parseInt(R2str)
+    console.log(R2);
+    const RCstr= urlParams.get("RC")
+    const RC = parseInt(RCstr)
+    console.log(RC);
+    const REstr = urlParams.get("RE")
+    const RE = parseInt(REstr)
+    console.log(RE);
+    const VCCstr = urlParams.get("VCC")
+    const VCC = parseInt(VCCstr)
+    console.log(VCC);
 
-    var vbb = VCC*(Res2/(Res1+Res2));
-    var ie = ((vbb-0.7)/ResE);
-    var ic = ie;
-    var ve = vbb-0.7;
-    var vc = VCC-ic*ResC;
-    var vce = vc-ve;
+        var vbb = VCC*(R2/(R1+R2));
+        var ie = ((vbb-0.7)/RE);
+        var ic = ie;
+        var ve = vbb-0.7;
+        var vc = VCC-ic*RC;
+        var vce = vc-ve;
 
-}
+    document.getElementById("vb").innerHTML = vbb;
+    document.getElementById("ie").innerHTML = ie;
+    document.getElementById("ic").innerHTML = ic;
+    document.getElementById("ve").innerHTML = ve;
+    document.getElementById("vc").innerHTML = vc;
+    document.getElementById("vce").innerHTML = vce;
